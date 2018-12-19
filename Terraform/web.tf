990becd5cc23cb1a54a1365b13de51647303277e
Nolan Venkiah <nolanve@fpnolvenmac.local>
2018-12-19 15:49:44 +0200

    web and security

---
+++ Terraform/web.tf
@@ -0,0 +1,24 @@
+/* -------------------------------------------------------
+These parameters creates an EC2 instance
+----------------------------------------------------------*/
+resource "aws_instance" "instance" {
+  ami                       = "${var.instance_ami}"
+  instance_type             = "${var.instance_type}"
+  subnet_id                 = "${var.subnet_public_id}"
+  vpc_security_group_ids    = ["${var.security_group_ids}"]
+  key_name                  = "${var.key_pair_name}"
+  tags {
+    "Environment"           = "${var.environment_tag}"
+  }
+}
+
+/* ----------------------------------------------------------
+These parameters creates a Elastic IP address for an instance
+------------------------------------------------------------*/
+resource "aws_eip" "EIP" {
+  vpc                       = true
+  instance                  = "${aws_instance.instance.id}"
+  tags {
+    "Environment"           = "${var.environment_tag}"
+  }
+}
